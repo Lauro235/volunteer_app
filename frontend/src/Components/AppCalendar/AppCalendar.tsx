7c7229e6bc69fc35673c53f18aa18db75df6bf14
Lauro235 <lorentzbloomguitar@gmail.com>
2023-09-29 11:34:33 +0100

    updated main

--- frontend/src/Components/AppCalendar/AppCalendar.tsx
+++ frontend/src/Components/AppCalendar/AppCalendar.tsx
@@ -1,15 +1,29 @@
 import { useState } from "react"
 import Calendar from "react-calendar"
+import { ISessionData } from "../../data/sessionData";
 
-const AppCalendar = () => {
+interface IAppCalendar {
+  sessions: ISessionData[];
+}
+
+const AppCalendar = ({sessions}: IAppCalendar) => {
   type ValuePiece = Date | null;
   type Value = ValuePiece | [ValuePiece, ValuePiece];
   const [value, onChange] = useState<Value>(new Date());
-  console.log(value);
+  
+  function dayHandler(e: React.MouseEvent<HTMLButtonElement>, value: Date) {
+    const dateString = (e.target as HTMLElement).ariaLabel
+    console.log(value);
+    
+
+    onChange(new Date(dateString !== null ? dateString : ''))
+    
+
+  }
   
   return (
     <>
-      <Calendar className="bg-blue-200 grid" onChange={(() => onChange(new Date('2023-11-13')))} value={value} />
+      <Calendar onClickDay={(value: Date, e: React.MouseEvent<HTMLButtonElement>) => dayHandler(e, value)} className="bg-blue-200 grid" value={value} />
     </>
   )
 }
