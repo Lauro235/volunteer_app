f4c2cac5f4cc60dccc3216ebac7f6bd822ed95ae
Lauro235 <lorentzbloomguitar@gmail.com>
2023-09-29 22:00:07 +0100

    center checkbox at desktop sizes

--- frontend/src/Components/AppCalendar/CalendarForm/CalendarSessionSlot.tsx
+++ frontend/src/Components/AppCalendar/CalendarForm/CalendarSessionSlot.tsx
@@ -2,14 +2,16 @@ interface ICalenderSessionSlot {
   slot: "morning" | "evening";
 }
 
-const CalendarSessionSlot = ({slot}: ICalenderSessionSlot) => {
+const CalendarSessionSlot = ({ slot }: ICalenderSessionSlot) => {
   return (
-    <div className="flex w-min sm:w-full justify-between">
-      <div>
+    <div className="flex w-min sm:gap-3 sm:w-full justify-between">
+      <div className="sm:flex-1 bg-gray-400">
         <p>{slot === "morning" ? "Morning" : "Evening"} slot</p>
         <p>available</p>
       </div>
-      <input type="checkbox" className="self-end" />
+      <div className="flex sm:flex-1 sm:items-center sm:justify-center bg-gray-400">
+        <input type="checkbox" className="self-end sm:self-center mb-2 sm:mb-0"/>
+      </div>
     </div>
   );
 };
